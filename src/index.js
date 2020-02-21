5e94839520b918b48c636d44de638482c8a76ebc
sungwon.cha <mygenie3@naver.com>
2020-02-21 17:09:44 +0900

    add autoFixJs feature

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { pugToJsx, babelTransform } from 'pug-as-jsx-utils';
+import { pugToJsx, babelTransform, codemod } from 'pug-as-jsx-utils';
 import { createFilter } from 'rollup-pluginutils';
 
 export default function pugAsJsx(options = {}) {
@@ -10,12 +10,15 @@ export default function pugAsJsx(options = {}) {
     transform(pugCode, id) {
       if (id.slice(-4) !== '.pug') return null;
       if (!filter(id)) return null;
-      const { jsxTemplate } = pugToJsx(pugCode, {
+      const { jsxTemplate, useThis, variables } = pugToJsx(pugCode, {
         template: true,
         resolve: options.resolve || {},
         rootDir: options.rootDir || undefined,
         resourcePath: id,
       });
+      if (options.autoUpdateJsFile) {
+        codemod({ useThis, variables }, id);
+      }
       const code = babelTransform(jsxTemplate, id);
       return { code };
     },
